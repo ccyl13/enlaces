2a2fea9daf00466e2a5834207e1366ce4e4ad1d3
Thomas O´neil Álvarez <168454163+ccyl13@users.noreply.github.com>
2025-05-28 13:46:35 +0200

    script.js

--- script.js
+++ script.js
@@ -1,74 +1,52 @@
-// Este script carga automáticamente el efecto de partículas desde un archivo de configuración remoto
-particlesJS('particles-js', {
-  particles: {
-    number: {
-      value: 60,
-      density: {
-        enable: true,
-        value_area: 800
-      }
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 60,
+      "density": { "enable": true, "value_area": 800 }
     },
-    color: {
-      value: "#ffffff"
+    "color": { "value": "#ffffff" },
+    "shape": {
+      "type": "circle",
+      "stroke": { "width": 0, "color": "#000000" },
+      "polygon": { "nb_sides": 5 }
     },
-    shape: {
-      type: "circle",
-      stroke: {
-        width: 0,
-        color: "#000000"
-      },
-      polygon: {
-        nb_sides: 5
-      }
+    "opacity": {
+      "value": 0.3,
+      "random": false,
+      "anim": { "enable": false }
     },
-    opacity: {
-      value: 0.3,
-      random: false,
-      anim: {
-        enable: false
-      }
+    "size": {
+      "value": 3,
+      "random": true
     },
-    size: {
-      value: 3,
-      random: true,
-      anim: {
-        enable: false
-      }
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.2,
+      "width": 1
     },
-    line_linked: {
-      enable: true,
-      distance: 150,
-      color: "#ffffff",
-      opacity: 0.2,
-      width: 1
-    },
-    move: {
-      enable: true,
-      speed: 1.2,
-      direction: "none",
-      random: false,
-      straight: false,
-      out_mode: "out"
+    "move": {
+      "enable": true,
+      "speed": 1.2,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false
     }
   },
-  interactivity: {
-    detect_on: "canvas",
-    events: {
-      onhover: {
-        enable: true,
-        mode: "repulse"
-      },
-      onclick: {
-        enable: false
-      },
-      resize: true
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": { "enable": true, "mode": "grab" },
+      "onclick": { "enable": true, "mode": "push" },
+      "resize": true
     },
-    modes: {
-      repulse: {
-        distance: 100,
-        duration: 0.4
-      }
+    "modes": {
+      "grab": { "distance": 140, "line_linked": { "opacity": 0.35 } },
+      "push": { "particles_nb": 2 }
     }
   },
-  retina_detect: true
+  "retina_detect": true
 });
